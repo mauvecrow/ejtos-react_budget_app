74fcece9c30cd59865a50fb9769b309eef07fe8c
Bradley Quangson <bradley.quangson@gmail.com>
2023-05-22 14:27:54 -0600

    finishlab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -24,6 +24,34 @@ const ExpenseItem = (props) => {
     });
   };
 
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
+
+    dispatch({
+      type: "RED_EXPENSE",
+      payload: expense,
+    });
+  };
+
+  const btnStyle = {
+    backgroundColor: "lightgreen",
+    color: "white",
+    borderRadius: "50%",
+    fontWeight: "bold",
+    borderColor: "white",
+  };
+
+  const btnStyle2 = {
+    backgroundColor: "red",
+    color: "white",
+    borderRadius: "50%",
+    fontWeight: "bold",
+    borderColor: "white",
+  };
+
   return (
     <tr>
       <td>{props.name}</td>
@@ -32,7 +60,20 @@ const ExpenseItem = (props) => {
         {props.cost}
       </td>
       <td>
-        <button onClick={(event) => increaseAllocation(props.name)}>+</button>
+        <button
+          style={btnStyle}
+          onClick={(event) => increaseAllocation(props.name)}
+        >
+          +
+        </button>
+      </td>
+      <td>
+        <button
+          style={btnStyle2}
+          onClick={(event) => decreaseAllocation(props.name)}
+        >
+          -
+        </button>
       </td>
       <td>
         <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
